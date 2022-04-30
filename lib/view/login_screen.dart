e491e330d4a5081ef5ec67f5a4855b190550fa8b
joao-vitor-machado <jv.muca@gmail.com>
2022-04-30 19:13:30 -0300

    configurações de rotas e cores

---
+++ lib/view/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const route = "/";
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      )
+    );
+  }
+}
