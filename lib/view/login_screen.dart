fb93161fc5d1b6bfb8dd67f26d23bdcf2320d4df
joao-vitor-machado <jv.muca@gmail.com>
2022-04-30 20:30:01 -0300

    primeiro commit sid

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -8,8 +8,35 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Column(
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  child: Image.asset("logo_v1.png")),
+              Text("Snacks n' Date"),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Form(child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "username"
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "password"
+                ),
+              ),
 
+
+            ],
+          ))
         ],
       )
     );
