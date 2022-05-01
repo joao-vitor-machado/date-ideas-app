77d83c02aaafbed705e5984696b46006478e3660
joao-vitor-machado <jv.muca@gmail.com>
2022-04-30 21:49:18 -0300

    login screen

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -4,41 +4,95 @@ class LoginScreen extends StatelessWidget {
   static const route = "/";
   const LoginScreen({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+    final screen = MediaQuery.of(context);
+    final tema = Theme.of(context).colorScheme;
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Container(
-                  alignment: Alignment.center,
-                  child: Image.asset("logo_v1.png")),
-              Text("Snacks n' Date"),
-            ],
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Form(child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "username"
-                ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "password"
-                ),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              children: [
+                Container(
+                    alignment: Alignment.center,
+                    child: Image.asset("logo_v1.png")),
+                Text("Snacks n' Date",
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontSize: 40,
+                  color: tema.primary,
+                ),),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: screen.size.width * 0.8,
+              child: Form(child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: tema.primary),
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      prefixIcon: Icon(Icons.account_circle),
+                      labelText: "username",
+                      border: OutlineInputBorder()
+
+                    ),
+                  ),
+                  SizedBox(
+                    height: screen.size.height * 0.02,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide:  BorderSide(color: tema.primary, width: 0.0),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: "password",
+                      border: OutlineInputBorder()
+                    ),
+                  ),
+                ],
+              )),
+            ),
 
 
-            ],
-          ))
-        ],
-      )
+          ],
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: tema.primary,
+                    minimumSize: const Size(
+                        200, 50
+                    )
+                ),
+                child: Text("Login",
+                  style: TextStyle(
+                      color: tema.secondary
+                  ),),
+                onPressed: (){}),
+          ],
+        ),
+      ),
     );
   }
 }
