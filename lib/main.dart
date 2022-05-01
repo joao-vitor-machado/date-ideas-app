1c174025587543274dfab4c226a649ecc779c2f9
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-05-01 19:50:17 -0300

    TelaPreferencias Finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho_fibal_mob_2022/view/login_screen.dart';
 import 'view/cadastro_screen.dart';
+import 'view/preferencias_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/cadastro",
+      initialRoute: "/preferencias",
       routes: {
         LoginScreen.route: (context) => const LoginScreen(),
         CadastroScreen.route: (context) => CadastroScreen(),
+        PreferenciasScreen.route: (context) => PreferenciasScreen(),
       },
       title: 'Flutter Demo',
       theme: tema.copyWith(
